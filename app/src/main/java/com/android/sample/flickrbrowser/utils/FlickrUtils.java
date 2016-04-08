279809fe95d432a685afceebe5f4132af712244d
Dat Nguyen <dat.nguyenthanhit@gmail.com>
2016-04-09 00:29:09 +0700

    add photo detail view, photo zoom

--- app/src/main/java/com/android/sample/flickrbrowser/utils/FlickrUtils.java
+++ app/src/main/java/com/android/sample/flickrbrowser/utils/FlickrUtils.java
@@ -11,7 +11,7 @@ public class FlickrUtils {
     public static final String FLICKR_API_KEY = "2b8fc75daa47791bab6b59b31f032f2f";
     public static final String FLICKR_SECRET_KEY = "46249e7bd2b9e808";
     public static final String FLICKR_BASE_URL = "https://api.flickr.com/services/rest/?";
-    public static final String APP_SCHEME = "com-android-sample-flickrbrowser";
+    public static final String APP_SCHEME = "com-android-flickrbrowser";
     public static final Uri OAUTH_CALLBACK_URI = Uri.parse(APP_SCHEME + "://oauth");
     public static final String PREFS_NAME = "com-android-sample-pref";
     public static final String KEY_OAUTH_TOKEN = "com-android-oauthToken";
